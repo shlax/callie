936cb77c5ed95f04eb1b6cb691f2b80229086aeb
slax <slax.10101@gmail.com>
2013-01-18 00:29:30 +0100

    .

--- callie-scala/src/org/callie/math/intr/Const.scala
+++ callie-scala/src/org/callie/math/intr/Const.scala
@@ -3,7 +3,7 @@ package org.callie.math.intr
 class Const(var v : Float) extends Intr{
   def this() = this(0)
   
-  override def set(value: Float){ v = value; }
+  override def := (value: Float){ v = value; }
 
   override def apply(t: Float): Float =  v
   
