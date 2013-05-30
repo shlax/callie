bc3e69b1f047cf6bd658493c7d7316a9b64e0596
shlax <slax.10101@gmail.com>
2013-05-30 13:54:33 +0200

    .

--- callie-scala/src/org/callie/ringing/JointCluster.scala
+++ callie-scala/src/org/callie/ringing/JointCluster.scala
@@ -1,11 +1,11 @@
 package org.callie.ringing
 
-import org.callie.math.Vector
-import org.callie.math.Matrix4f
+import org.callie.math.Vector3
+import org.callie.math.Matrix4
 
-class JointCluster(val points:Traversable[(Vector, Vector)], val normals:Traversable[(Vector, Vector)]){
+class JointCluster(val points:Traversable[(Vector3, Vector3)], val normals:Traversable[(Vector3, Vector3)]){
   
-  def apply(p:Matrix4f, n:Matrix4f){
+  def apply(p:Matrix4, n:Matrix4){
     for(v <- points) p(v._1, v._2)
     for(v <- normals) n(v._1, v._2)
   }
