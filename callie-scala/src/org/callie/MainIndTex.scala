f0c6ca092bfca21058eb78374d6c976631acf383
shlax <slax.10101@gmail.com>
2013-09-18 13:31:49 +0200

    .

--- callie-scala/src/org/callie/MainIndTex.scala
+++ callie-scala/src/org/callie/MainIndTex.scala
@@ -1,9 +1,9 @@
 package org.callie
 
-import org.callie.jogl.{buffers, GL_4, GL4EventListener, JoglFrame}
 import javax.media.opengl.GL4
-import com.jogamp.opengl.util.texture.{Texture, TextureIO}
-import java.io.FileInputStream
+import com.jogamp.opengl.util.texture.TextureIO
+import com.jogamp.common.nio.Buffers
+import org.callie.jogl.{buffers, GL_4, GL4EventListener, JoglFrame}
 import buffers._
 
 object MainIndTex extends App{
@@ -34,16 +34,17 @@ object MainIndTex extends App{
       |out vec4 outputColor;
       |
       |void main(){
+      |   //outputColor = vec4(passTextureCoord[0], passTextureCoord[1], 1.0f, 1.0f);
       |   outputColor = texture(textureDiffuse, passTextureCoord);
       |}
     """.stripMargin
 
   val positions = Array(
   //position (3)        texture coord (2)
-    -0.75f,  0.75f, 0f, -1f,  1f,
-    -0.75f, -0.75f, 0f, -1f, -1f,
-     0.75f, -0.75f, 0f,  1f, -1f,
-     0.75f,  0.75f, 0f,  1f,  1f
+    -0.75f,  0.75f, 0f, 0f, 1f,
+    -0.75f, -0.75f, 0f, 0f, 0f,
+     0.75f, -0.75f, 0f, 1f, 0f,
+     0.75f,  0.75f, 0f, 1f, 1f
   )
 
   val indices = Array(
@@ -54,17 +55,33 @@ object MainIndTex extends App{
   )
 
   JoglFrame(new GL4EventListener(){
-    var texture : Texture = _
+    //var texture : TextureData = _
+    var texId : Int = _
 
     var vao : Int = _
     var vbi : Int = _
 
     override def initGL4(implicit gl: GL4) {
-      texture = TextureIO.newTexture(new FileInputStream("t.png"), true, TextureIO.PNG)
-      texture.enable(gl)
+
+      val texture = TextureIO.newTextureData(gl.getGLProfile, getClass.getResourceAsStream("/t.png"), false, TextureIO.PNG)
+
+      gl.glActiveTexture(GL_4.TEXTURE0)
+
+      texId = &(gl.glGenTextures(1, _, 0))
+      gl.glBindTexture(GL_4.TEXTURE_2D, texId)
+
+      gl.glPixelStorei(GL_4.TEXTURE_2D, texture.getAlignment)
+      gl.glTexImage2D(GL_4.TEXTURE_2D, 0, texture.getInternalFormat, texture.getWidth, texture.getHeight, texture.getBorder, texture.getPixelFormat, texture.getPixelType, texture.getBuffer)
+
+      gl.glGenerateMipmap(GL_4.TEXTURE_2D)
+
+      gl.glTexParameteri(GL_4.TEXTURE_2D, GL_4.TEXTURE_WRAP_S, GL_4.REPEAT)
+      gl.glTexParameteri(GL_4.TEXTURE_2D, GL_4.TEXTURE_WRAP_T, GL_4.REPEAT)
+      gl.glTexParameteri(GL_4.TEXTURE_2D, GL_4.TEXTURE_MAG_FILTER, GL_4.LINEAR)
+      gl.glTexParameteri(GL_4.TEXTURE_2D, GL_4.TEXTURE_MIN_FILTER, GL_4.LINEAR_MIPMAP_LINEAR)
 
       val p = createProgram(createShader(GL_4.VERTEX_SHADER, vertex),
-        createShader(GL_4.FRAGMENT_SHADER, fragment))
+                            createShader(GL_4.FRAGMENT_SHADER, fragment))
 
       // > code
       vao = createVertexArray{
@@ -75,8 +92,8 @@ object MainIndTex extends App{
         createBuffer(GL_4.ARRAY_BUFFER){
           positions.asBuffer(gl.glBufferData(GL_4.ARRAY_BUFFER, _, _, GL_4.STATIC_DRAW))
 
-          gl.glVertexAttribPointer(0, 3, GL_4.FLOAT, false, 3 + 2, 3)
-          gl.glVertexAttribPointer(0, 2, GL_4.FLOAT, false, 3 + 2, 3)
+          gl.glVertexAttribPointer(0, 3, GL_4.FLOAT, false, (3+2)*Buffers.SIZEOF_FLOAT, 0*Buffers.SIZEOF_FLOAT)
+          gl.glVertexAttribPointer(1, 2, GL_4.FLOAT, false, (3+2)*Buffers.SIZEOF_FLOAT, 3*Buffers.SIZEOF_FLOAT)
         }
 
       }
@@ -94,10 +111,10 @@ object MainIndTex extends App{
 
     override def displayGL4(implicit gl: GL4) {
       gl.glClear(GL_4.COLOR_BUFFER_BIT)
-
       // > code
       //gl.glDrawArrays(GL_4.TRIANGLES, 0, 3)
-      texture.bind(gl)
+
+      gl.glBindTexture(GL_4.TEXTURE_2D, texId)
 
       bindVertexArray(vao){
         bindBuffer(GL_4.ELEMENT_ARRAY_BUFFER, vbi){
