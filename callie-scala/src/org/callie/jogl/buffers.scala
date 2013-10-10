2c95feb36ba04356137fc0e87fd327746570939d
slax <slax.10101@gmail.com>
2013-10-10 05:45:20 +0200

    .

--- callie-scala/src/org/callie/jogl/buffers.scala
+++ callie-scala/src/org/callie/jogl/buffers.scala
@@ -6,18 +6,20 @@ import com.jogamp.common.nio.Buffers
 object buffers{
 
   implicit class IntArray(a:Array[Int]){
-    def asBuffer(f:(Int, IntBuffer) => Unit){
+    def asBuffer(f:(Int, IntBuffer) => Unit) = {
       val buff = Buffers.newDirectIntBuffer(a.length)
       buff.put(a); buff.rewind()
       f(a.length * Buffers.SIZEOF_INT, buff)
+      buff
     }
   }
 
   implicit class FloatArray(a:Array[Float]){
-    def asBuffer(f:(Int, FloatBuffer) => Unit){
+    def asBuffer(f:(Int, FloatBuffer) => Unit) = {
       val buff = Buffers.newDirectFloatBuffer(a.length)
       buff.put(a); buff.rewind()
       f(a.length * Buffers.SIZEOF_FLOAT, buff)
+      buff
     }
   }
 }
