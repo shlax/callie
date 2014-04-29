9f41b9f2d5e542364dd61d9366c6ca1ee72fe6a9
shlax <slax.10101@gmail.com>
2014-04-29 16:44:35 +0200

    .

--- callie-scala/src/org/callie/input/Inputs.scala
+++ callie-scala/src/org/callie/input/Inputs.scala
@@ -3,21 +3,82 @@ package org.callie.input
 import java.awt.event.{MouseEvent, KeyEvent, MouseListener, KeyListener}
 
 object Inputs extends KeyListener with MouseListener{
+
+  def hasKey(k:Int) = lockKb.synchronized{ keys.contains(k) }
+  def hasButton(b:Int) = lockMb.synchronized{ buttons.companion(b) }
+
+  def key() = lockKe.synchronized{
+    keyEvents match {
+      case h :: t =>
+        keyEvents = t
+        Some(h)
+      case Nil => None
+    }
+  }
+
+  def button() = lockMe.synchronized{
+    mouseEvents match {
+      case h :: t =>
+        mouseEvents = t
+        Some(h)
+      case Nil => None
+    }
+  }
+
+  // impl
+
+  var lockMb = new Object
+  var lockMe = new Object
+
+  var buttons = Set[Int]()
+  var mouseEvents = List[Int]()
+
   // mouse
-  override def mouseExited(e: MouseEvent){}
 
-  override def mouseEntered(e: MouseEvent){}
+  override def mouseReleased(e: MouseEvent){
+    lockMb.synchronized{
+      buttons = buttons - e.getButton
+    }
+  }
 
-  override def mouseReleased(e: MouseEvent){}
+  override def mousePressed(e: MouseEvent){
+    lockMb.synchronized {
+      buttons = buttons + e.getButton
+    }
+  }
 
-  override def mousePressed(e: MouseEvent){}
+  override def mouseClicked(e: MouseEvent){
+    lockMe.synchronized {
+      mouseEvents = mouseEvents :+ e.getButton
+    }
+  }
 
-  override def mouseClicked(e: MouseEvent){}
+  var lockKb = new Object
+  var lockKe = new Object
+
+  var keys = Set[Int]()
+  var keyEvents = List[Int]()
 
   // key
-  override def keyReleased(e: KeyEvent){}
+  override def keyReleased(e: KeyEvent){
+    lockKb.synchronized {
+      keys = keys - e.getKeyCode
+    }
+  }
 
-  override def keyPressed(e: KeyEvent){}
+  override def keyPressed(e: KeyEvent){
+    lockKb.synchronized {
+      keys = keys + e.getKeyCode
+    }
+  }
 
-  override def keyTyped(e: KeyEvent){}
+  override def keyTyped(e: KeyEvent){
+    lockKe.synchronized {
+      keyEvents = keyEvents :+ e.getKeyCode
+    }
+  }
+
+  // none
+  override def mouseExited(e: MouseEvent){}
+  override def mouseEntered(e: MouseEvent){}
 }
