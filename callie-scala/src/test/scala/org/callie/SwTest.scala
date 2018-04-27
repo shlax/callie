141061a89e393902152a3c6f7162a2988c3bae59
slax <slax.10101@gmail.com>
2018-04-27 06:07:53 +0200

    .

--- callie-scala/src/test/scala/org/callie/SwTest.scala
+++ callie-scala/src/test/scala/org/callie/SwTest.scala
@@ -1,119 +1,31 @@
 package org.callie
 
+import java.io.FileOutputStream
+
+import org.apache.commons.io.IOUtils
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+
 import scala.language.implicitConversions
-import scala.concurrent.{ExecutionContextExecutor, ExecutionContext}
-import java.util.concurrent.{Executor, Executors}
-import javax.swing.SwingUtilities
 
-object SwTest extends App{
+object SwTest extends App {
 
-  def g1() = {
-    println("2"+Thread.currentThread().getName)
-    4
-  }
+  val c = HttpClientBuilder.create().build()
 
-  def j1(i:Int) = {
-    println("3"+Thread.currentThread().getName)
+  for(i <- 3654 until 5000){
+    val r = c.execute(new HttpGet("http://try-it-companion.com/data/slecny/slecna-"+i+"-3.jpg"))
+    if(r.getStatusLine.getStatusCode == 200) {
+      val o = new FileOutputStream("c:\\Users\\root\\Desktop\\b\\cse\\dt\\" + i + ".jpg")
+      IOUtils.copy(r.getEntity.getContent, o)
+      r.close()
+      o.close()
+    }else{
+      r.close()
+      print("x")
+    }
+
+    Thread.sleep(250)
     println(i)
-    7
-  }
-
-  def g2(a:Int) = {
-    println(a+"/"+Thread.currentThread().getName)
-    (j:Int) => {
-      println("4"+Thread.currentThread().getName+""+j)
-      5
-    }
-  }
-
-  println("1"+Thread.currentThread().getName)
-
-  Command.gui(g1()).job(j1).gui(g2(9)).job{ i =>
-    println("5"+Thread.currentThread().getName)
-    println(i)
-  }.run()
-
-  Command.job{
-    println("-3"+Thread.currentThread().getName)
-    7
-  }.gui{ j =>
-    println("-4"+Thread.currentThread().getName+""+j)
-    5
-  }.gui{ j =>
-    println("-5"+Thread.currentThread().getName+""+j)
-    5
-  }.run()
-
-}
-
-object Command{
-  implicit def asRun[F](f: => F):Runnable = new Runnable(){ def run() { f } }
-
-  val ecJob = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
-  val ecGui = ExecutionContext.fromExecutor(new Executor(){
-    override def execute(command: Runnable){ SwingUtilities.invokeLater(command) }
-  })
-
-  def gui[T](f : => T) = new GuiCommand[T](f)
-  def job[T](f : => T) = new JobCommand[T](f)
-}
-
-trait Command[T] extends Runnable{
-  var next : Option[Task[T, _]] = None
-
-  def map[V](t:Task[T, V]) = {
-    next = Some(t)
-    t
-  }
-
-  def gui[V](nf : T => V) = map(new GuiTask(this, nf))
-  def job[V](nf : T => V) = map(new JobTask(this, nf))
-
-  def execute(t: Task[T, _],b:T)
-}
-
-trait CommandGui[T] extends Command[T]{
-  override def execute(t: Task[T, _], v:T){
-    import Command._
-    t match {
-      case g: GuiTask[T, _] => g.complete(v)
-      case j: JobTask[T, _] => Command.ecJob.execute(j.complete(v))
-    }
-  }
-}
-
-trait CommandJob[T] extends Command[T]{
-  override def execute(t: Task[T, _], v:T){
-    import Command._
-    t match {
-      case g: GuiTask[T, _] => Command.ecGui.execute(g.complete(v))
-      case j: JobTask[T, _] => j.complete(v)
-    }
-  }
-}
-
-abstract class BaseCommand[T](f: => T, ec:ExecutionContextExecutor) extends Command[T]{
-  override def run() {
-    import Command._
-    ec.execute(asRun{
-      val v = f
-      next.foreach(execute(_, v))
-    })
-  }
-}
-
-class GuiCommand[T](f: => T) extends BaseCommand[T](f, Command.ecGui) with CommandGui[T]
-class JobCommand[T](f: => T) extends BaseCommand[T](f, Command.ecJob) with CommandJob[T]
-
-abstract class Task[A, B](c: Command[_], f: A => B) extends Command[B]{
-  override def run(){ c.run() }
-
-  def complete(a: A){
-    val v = f(a)
-    next.foreach(execute(_, v))
   }
 
 }
-
-class GuiTask[A, B](c: Command[_], f: A => B) extends Task[A, B](c, f) with CommandGui[B]
-class JobTask[A, B](c: Command[_], f: A => B) extends Task[A, B](c, f) with CommandJob[B]
