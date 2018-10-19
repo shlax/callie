1baf64d94ef9ffa88fa420eb26fb855424751c78
Ochodnicky Peter <pochodnicky@gratex.com>
2018-10-19 14:55:18 +0200

    something 2

--- callie-scala/src/main/scala/org/callie/model/Physics.scala
+++ callie-scala/src/main/scala/org/callie/model/Physics.scala
@@ -1,30 +1,30 @@
-package org.callie.model
-
-trait Physics {
-  /** seconds */
-  def update(deltaTime:Float)
-}
-
-object Physics{
-  var time :Float = _
-  var parts:Array[Physics] = Array()
-
-  def register(e:Physics*){
-    parts = Array.concat(parts, e.toArray)
-  }
-
-  var startTime:Long = _
-
-  def init(){
-    startTime = System.currentTimeMillis()
-    time = startTime
-  }
-
-  def update(){
-    val t = (System.currentTimeMillis() - startTime).asInstanceOf[Float] / 1000000f
-    val dt = t - time
-    time = t
-    for(e <- parts) e.update(dt)
-  }
-
-}
+//package org.callie.model
+//
+//trait Physics {
+//  /** seconds */
+//  def update(deltaTime:Float)
+//}
+//
+//object Physics{
+//  var time :Float = _
+//  var parts:Array[Physics] = Array()
+//
+//  def register(e:Physics*){
+//    parts = Array.concat(parts, e.toArray)
+//  }
+//
+//  var startTime:Long = _
+//
+//  def init(){
+//    startTime = System.currentTimeMillis()
+//    time = startTime
+//  }
+//
+//  def update(){
+//    val t = (System.currentTimeMillis() - startTime).asInstanceOf[Float] / 1000000f
+//    val dt = t - time
+//    time = t
+//    for(e <- parts) e.update(dt)
+//  }
+//
+//}
