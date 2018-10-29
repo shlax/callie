ffdfddcf05960d6a57f5c252bed86ba97b2158a8
slax <slax.10101@gmail.com>
2018-10-29 21:47:40 +0100

    .

--- callie-scala/src/main/scala/org/callie/math/intr/Accl.scala
+++ callie-scala/src/main/scala/org/callie/math/intr/Accl.scala
@@ -1,15 +1,26 @@
 package org.callie.math.intr
 
 /*
-    v1 = v0 + a1/2
-    v2 = v1 + a2/2
+    v1 = v0 + a1*t1
+    v2 = v1 + a2*(1-t1)
 
-    s1 = v0/2 + a1/8
-    s2 = s1 + v1/2 + a2/8
+    s1 = v0*t1 + a1*t1*t1/2
+    s2 = s1 + v1*(1-t1) + a2*(1-t1)*(1-t1)/2
+
+    =>
+
+    a2 = (v1 - v2)/(t1 - 1)
+
+    s2 = v0*t1 + a1*t1*t1/2 + (v0 + a1*t1)*(1-t1) + (((v0 + a1*t1) - v2)/(t1 - 1))*(1-t1)*(1-t1)/2
+
+    a1 = -(-2*s2 + t1*v0 - t1*v2 + v0 + v2)/t1
+
 */
 class Accl extends Intr{
   var lastS = 0f
   var lastV = 0f
+
+  var t1 = 0.5f
 
   var v0 = 0f
   var v1 = 0f
@@ -37,28 +48,34 @@ class Accl extends Intr{
     }else { */
       // active = true
     val s3 = nextValue - value
-    val v2 = if(s3 != 0f && Math.signum(s2) == Math.signum(s3)) nextValue - s0 else 0f
+    val v2 = if(s3 != 0f && Math.signum(s2) == Math.signum(s3)){
+      t1 = 0.5f
+      nextValue - s0
+    } else{
+      t1 = 0.5f
+      0f
+    }
 
     v0 = lastV * rescale
 
-    a1 = 4 * s2 - 3 * v0 - v2
-    v1 = v0 + a1 / 2
+    a1 = -(-2*s2 + t1*v0 - t1*v2 + v0 + v2)/t1
+    v1 = v0 + a1*t1
 
-    a2 = 2 * v2 - 2 * v1
+    a2 = (v1 - v2)/(t1 - 1)
 
-    s1 = v0 / 2 + a1 / 8
+    s1 = v0*t1 + a1*t1*t1/2
     // }
   }
 
   override def apply(t: Float): Float = {
     // if(!active) return s0
 
-    if(t <= 0.5f){
+    if(t <= t1){
       val at = a1 * t
       lastV = v0 + at
       lastS = v0 * t + (at * t)/2
     }else{
-      val dt = t - 0.5f
+      val dt = t - t1
       val adt = a2 * dt
       lastV = v1 + adt
       lastS = v1 * dt + (adt * dt)/2 + s1
