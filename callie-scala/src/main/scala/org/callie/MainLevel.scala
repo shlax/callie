415e21d04e94173b4a50bdd8a632b82c1d1bc6d9
Ochodnicky Peter <pochodnicky@gratex.com>
2019-08-02 09:22:47 +0200

    clean up

--- callie-scala/src/main/scala/org/callie/MainLevel.scala
+++ callie-scala/src/main/scala/org/callie/MainLevel.scala
@@ -2,7 +2,7 @@ package org.callie
 
 import org.callie.control.MovingObject
 import org.callie.input.Camera
-import org.callie.ogl.{Gl, GlEventListener, LwglFrame, Scene}
+import org.callie.ogl.{Gl, GlEventListener, LwglFrame}
 import org.callie.map.Map25
 import org.callie.math.Vector3
 import org.callie.model.{GlObject, Mod, MorfingObject, StaticObject, TextureGroup}
@@ -10,95 +10,94 @@ import org.callie.ringing.{JoinControl, Joint, JointAttachment, KeyFrameLoader, 
 
 object MainLevel extends App {
 
-  LwglFrame{gl: GlEventListener => new Scene{
-      val camCtrl = new MovingObject(Map25.load("/data/map/data.map"), 1.65f)
-      Camera.lookAt(camCtrl)
+  LwglFrame{gl: GlEventListener =>
+    val camCtrl = new MovingObject(Map25.load("/data/map/data.map"), 1.65f)
+    Camera.lookAt(camCtrl)
 
-      val (charObj: Array[MorfingObject], joint: Joint, zero: Vector3) = Node.load(gl, Map(
-        "pSphere5" -> Mod.load("/data/char/model.mod").scale(0.1f),
-        "polySurface115" -> Mod.load("/data/char/hair.mod").scale(0.1f)
-      ), "/data/char/joints.skl", 0.1f)
+    val (charObj: Array[MorfingObject], joint: Joint, zero: Vector3) = Node.load(gl, Map(
+      "pSphere5" -> Mod.load("/data/char/model.mod").scale(0.1f),
+      "polySurface115" -> Mod.load("/data/char/hair.mod").scale(0.1f)
+    ), "/data/char/joints.skl", 0.1f)
 
-      val stand = KeyFrameLoader.load(joint, "/data/char/anim/stand.ang", 0.1f)
-      val run = for (i <- 1 to 8) yield KeyFrameLoader.load(joint, "/data/char/anim/run/run" + i + ".ang", 0.1f)
+    val stand = KeyFrameLoader.load(joint, "/data/char/anim/stand.ang", 0.1f)
+    val run = for (i <- 1 to 8) yield KeyFrameLoader.load(joint, "/data/char/anim/run/run" + i + ".ang", 0.1f)
 
-      val anim: JoinControl = JoinControl(camCtrl, joint, zero, stand, run: _*)
+    val anim: JoinControl = JoinControl(camCtrl, joint, zero, stand, run: _*)
 
-      //    val leg = joint.lookup("joint752").get.asInstanceOf[JointAttachment]
-      //    val hand = joint.lookup("joint714").get.asInstanceOf[JointAttachment]
+    //    val leg = joint.lookup("joint752").get.asInstanceOf[JointAttachment]
+    //    val hand = joint.lookup("joint714").get.asInstanceOf[JointAttachment]
 
 
 
-      /* val sphere = new TextureGroup(this, "/demo/box/white.png", Gl.TEXTURE0,
-      new StaticObject(this, Mod.load("/demo/box/sphere.mod"))
-    ) */
+    /* val sphere = new TextureGroup(this, "/demo/box/white.png", Gl.TEXTURE0,
+    new StaticObject(this, Mod.load("/demo/box/sphere.mod"))
+  ) */
 
-      val objects = Array[GlObject](
-        new TextureGroup(gl, "/data/char/char.png", Gl.GL_TEXTURE0, charObj.toIndexedSeq: _*),
+    val objects = Array[GlObject](
+      new TextureGroup(gl, "/data/char/char.png", Gl.GL_TEXTURE0, charObj.toIndexedSeq: _*),
 
-        //new TextureGroup(this, "/demo/t.png", Gl.TEXTURE0,
-        new TextureGroup(gl, "/data/map/sand.png", Gl.GL_TEXTURE0,
-          new StaticObject(gl, Mod.load("/data/map/dunes.mod"))
-        ),
+      //new TextureGroup(this, "/demo/t.png", Gl.TEXTURE0,
+      new TextureGroup(gl, "/data/map/sand.png", Gl.GL_TEXTURE0,
+        new StaticObject(gl, Mod.load("/data/map/dunes.mod"))
+      ),
 
-        new TextureGroup(gl, "/data/map/sky.png", Gl.GL_TEXTURE0,
-          new StaticObject(gl, Mod.load("/data/map/sky.mod"))
-        ),
+      new TextureGroup(gl, "/data/map/sky.png", Gl.GL_TEXTURE0,
+        new StaticObject(gl, Mod.load("/data/map/sky.mod"))
+      ),
 
-        new TextureGroup(gl, "/data/map/sun.png", Gl.GL_TEXTURE0,
-          new StaticObject(gl, Mod.load("/data/map/sun.mod"))
-        )
+      new TextureGroup(gl, "/data/map/sun.png", Gl.GL_TEXTURE0,
+        new StaticObject(gl, Mod.load("/data/map/sun.mod"))
       )
+    )
 
-      val prog = gl.createProgram(
-        gl.createShader(Gl.GL_VERTEX_SHADER, GlPrograms.vertex()),
-        gl.createShader(Gl.GL_FRAGMENT_SHADER, GlPrograms.fragment(discard = true))
+    val prog = gl.createProgram(
+      gl.createShader(Gl.GL_VERTEX_SHADER, GlPrograms.vertex()),
+      gl.createShader(Gl.GL_FRAGMENT_SHADER, GlPrograms.fragment(discard = true))
+    )
+
+    //char.init(gl)
+
+    //sphere.init(gl)
+    val camProg = Camera.program(prog)
+
+    val revolver = new TextureGroup(gl, "/data/char/pistol/revolver.png", Gl.GL_TEXTURE0,
+      JointAttachment(camProg, joint, "joint752",
+        new StaticObject(gl, Mod.load("/data/char/pistol/holder.mod").scale(0.1f))
       )
+    )
 
-      //char.init(gl)
+    revolver.init()
 
-      //sphere.init(gl)
-      val camProg = Camera.program(prog)
+    for (o <- objects) o.init()
 
-      val revolver = new TextureGroup(gl, "/data/char/pistol/revolver.png", Gl.GL_TEXTURE0,
-        JointAttachment(camProg, joint, "joint752",
-          new StaticObject(gl, Mod.load("/data/char/pistol/holder.mod").scale(0.1f))
-        )
-      )
+    Gl.glUseProgram(prog)
+    Gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+    Gl.glEnable(Gl.GL_DEPTH_TEST)
 
-      revolver.init()
+    //Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_LINE)
 
-      for (o <- objects) o.init()
+    var t = System.nanoTime()
 
-      Gl.glUseProgram(prog)
-      Gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
-      Gl.glEnable(Gl.GL_DEPTH_TEST)
+    () => {
+      Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT)
 
-      //Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_LINE)
+      Camera.apply()
 
-      var t = System.nanoTime()
+      val tmp = System.nanoTime()
+      val dt: Float = ((tmp - t) / 1e9d).asInstanceOf[Float]
+      t = tmp
 
-      override def update(): Unit = {
-        Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT)
+      anim.apply(dt)
+      //char.update(gl)
+      //sphere.update(gl)
 
-        Camera.apply()
+      camProg.light()
 
-        val tmp = System.nanoTime()
-        val dt: Float = ((tmp - t) / 1e9d).asInstanceOf[Float]
-        t = tmp
+      revolver.update()
 
-        anim.apply(dt)
-        //char.update(gl)
-        //sphere.update(gl)
-
-        camProg.light()
-
-        revolver.update()
-
-        camProg.identity()
-        for (o <- objects) o.update()
-      }
-
+      camProg.identity()
+      for (o <- objects) o.update()
     }
+
   }
 }
