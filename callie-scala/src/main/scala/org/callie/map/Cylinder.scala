2d97eda130b389b8658f4bbc08e1d23703ac76dc
Ochodnicky Peter <pochodnicky@gratex.com>
2019-10-29 16:49:18 +0100

    polar mapping

--- callie-scala/src/main/scala/org/callie/map/Cylinder.scala
+++ callie-scala/src/main/scala/org/callie/map/Cylinder.scala
@@ -1,29 +1,69 @@
 package org.callie.map
 
 import java.util.function.Predicate
-
 import org.callie.math.{Axis, Vector3}
+
 import scala.math.Ordering.Float.TotalOrdering
 
-class Cylinder(a:Axis, points: List[Vector3]) extends Predicate[(Int, Int)]{
+class Cylinder(a:Axis, up:Axis, points: List[Vector3]) extends Predicate[(Int, Int)]{
+  val pi = Math.PI.asInstanceOf[Float]
 
-  val min = points.map{ t =>
-    a match {
-      case Axis.X => t.x
-      case Axis.Y => t.y
-      case Axis.Z => t.z
+  def value(t:Vector3) = a match {
+    case Axis.X => t.x
+    case Axis.Y => t.y
+    case Axis.Z => t.z
+  }
+
+  def valueUp(t:Vector3) = up match {
+    case Axis.X => t.x
+    case Axis.Y => t.y
+    case Axis.Z => t.z
+  }
+
+  val min = points.map(value).min
+  val max = points.map(value).max
+
+  val len = max - min
+  val koef = 2f * pi / len
+  val radius = len / (2f * pi)
+
+  def angle(p:Vector3) = {
+    val v = value(p)
+    -pi + (v - min) * koef
+  }
+
+  val polar = points.map{ p =>
+    val ang = angle(p)
+    val r = radius - valueUp(p)
+
+    val x = r * Math.cos(ang).asInstanceOf[Float]
+    val y = r * Math.sin(ang).asInstanceOf[Float]
+
+    val pol = a match {
+      case Axis.X =>
+        up match {
+          case Axis.Y =>
+            Vector3(x, y, p.z)
+          case Axis.Z =>
+            Vector3(x, p.y, y)
+        }
+      case Axis.Y =>
+        up match {
+          case Axis.X =>
+            Vector3(y, x, p.z)
+          case Axis.Z =>
+            Vector3(p.x, x, y)
+        }
+      case Axis.Z =>
+        up match {
+          case Axis.X =>
+            Vector3(y, p.y, x)
+          case Axis.Y =>
+            Vector3(p.x, y, x)
+        }
     }
-  }.min
-
-  val max = points.map{ t =>
-    a match {
-      case Axis.X => t.x
-      case Axis.Y => t.y
-      case Axis.Z => t.z
-    }
-  }.max
-
-
+    (p, pol)
+  }
 
   override def test(t: (Int, Int)): Boolean = {
     val epsilon = 0.0001f
@@ -54,10 +94,10 @@ class Cylinder(a:Axis, points: List[Vector3]) extends Predicate[(Int, Int)]{
 
 }
 
-class CylinderBuilder(a:Axis) extends MapBuilder{
+class CylinderBuilder(a:Axis, up:Axis) extends MapBuilder{
 
   override def apply(points: List[Vector3]): Cylinder = {
-    new Cylinder(a, points)
+    new Cylinder(a, up, points)
   }
 
 }
