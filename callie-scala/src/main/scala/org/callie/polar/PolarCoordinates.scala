0c77fc592532961e84cb738afe23bddd9cbf334b
Ochodnicky Peter <pochodnicky@gratex.com>
2021-07-29 09:31:58 +0200

    PolarCoordinates

--- callie-scala/src/main/scala/org/callie/polar/PolarCoordinates.scala
+++ callie-scala/src/main/scala/org/callie/polar/PolarCoordinates.scala
@@ -2,14 +2,14 @@ package org.callie.polar
 
 import org.callie.control.{MovingControl, MovingState}
 import org.callie.input.TrackingObject
-import org.callie.math.{Matrix4, Vector3}
+import org.callie.math.{Angle, Matrix4, Vector3}
 
 class PolarCoordinates extends TrackingObject with MovingControl{
 
   override val position = Vector3()
 
-  var angle:Float = 0f
-  var radius:Float = 0f
+  var a = Angle()
+  var r:Float = 0f
 
   var z:Float = 0f
 
